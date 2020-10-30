6899c272848a8a0c147fccd5b9c5a30d02241f59
Dong He <donghe@cs.washington.edu>
2020-10-30 18:21:41 -0500

    finish maxpool layer

--- src/maxpool_layer.c
+++ src/maxpool_layer.c
@@ -20,10 +20,28 @@ matrix forward_maxpool_layer(layer l, matrix in)
     int outh = (l.height-1)/l.stride + 1;
     matrix out = make_matrix(in.rows, outw*outh*l.channels);
 
-    // TODO: 6.1 - iterate over the input and fill in the output with max values
+    // DONE: 6.1 - iterate over the input and fill in the output with max values
+    for(int r = 0; r < in.rows; ++r) {
+        int c = r * out.cols;
+        for (int ch = 0; ch < l.channels; ++ch) {
+            for (int row = 0; row < l.height; row += l.stride) {
+                for (int col = 0; col < l.width; col += l.stride) {
+                    float val_max = -1e18;
+                    for (int i = -(l.size - 1) / 2; i < -(l.size - 1) / 2 + l.size; ++i) {
+                        for (int j = -(l.size - 1) / 2; j < -(l.size - 1) / 2 + l.size; ++j) {
+                            if (row + i >= 0 && row + i < l.height && col + j >= 0 && col + j < l.width) {
+                                float candidate = in.data[ch * l.height * l.width + r * in.cols + (row + i) * l.width + col + j];
+                                if (candidate > val_max)
+                                    val_max = candidate;
+                            }
 
-
-
+                        }
+                    }
+                    out.data[c++] = val_max;
+                }
+            }
+        }
+    }
     return out;
 }
 
@@ -35,14 +53,33 @@ matrix backward_maxpool_layer(layer l, matrix dy)
     matrix in    = *l.x;
     matrix dx = make_matrix(dy.rows, l.width*l.height*l.channels);
 
-    int outw = (l.width-1)/l.stride + 1;
-    int outh = (l.height-1)/l.stride + 1;
-    // TODO: 6.2 - find the max values in the input again and fill in the
+    // DONE: 6.2 - find the max values in the input again and fill in the
     // corresponding delta with the delta from the output. This should be
     // similar to the forward method in structure.
+    for(int r = 0; r < in.rows; ++r) {
+        int c = r * dy.cols;
+        for (int ch = 0; ch < l.channels; ++ch) {
+            for (int row = 0; row < l.height; row += l.stride) {
+                for (int col = 0; col < l.width; col += l.stride) {
+                    float val_max = -1e18;
+                    int val_max_pos = -1;
+                    for (int i = -(l.size - 1) / 2; i < -(l.size - 1) / 2 + l.size; ++i) {
+                        for (int j = -(l.size - 1) / 2; j < -(l.size - 1) / 2 + l.size; ++j) {
+                            if (row + i >= 0 && row + i < l.height && col + j >= 0 && col + j < l.width) {
+                                float candidate = in.data[ch * l.height * l.width + r * in.cols + (row + i) * l.width + col + j];
+                                if (candidate > val_max) {
+                                    val_max = candidate;
+                                    val_max_pos = l.width * row + col + i * l.width + j + ch * l.height * l.width + r * in.cols;
+                                }
+                            }
 
-
-
+                        }
+                    }
+                    dx.data[val_max_pos] += dy.data[c++];
+                }
+            }
+        }
+    }
     return dx;
 }
 
