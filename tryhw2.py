eb09dfd91642634d4bd3db9998910a146adfe27e
Dong He <donghe@cs.washington.edu>
2020-11-16 11:32:14 -0600

    done

--- tryhw2.py
+++ tryhw2.py
@@ -1,12 +1,15 @@
 from uwnet import *
 def conv_net():
     l = [   make_convolutional_layer(32, 32, 3, 8, 3, 2),
+            make_batchnorm_layer(8),
             make_activation_layer(RELU),
             make_maxpool_layer(16, 16, 8, 3, 2),
             make_convolutional_layer(8, 8, 8, 16, 3, 1),
+            make_batchnorm_layer(16),
             make_activation_layer(RELU),
             make_maxpool_layer(8, 8, 16, 3, 2),
             make_convolutional_layer(4, 4, 16, 32, 3, 1),
+            make_batchnorm_layer(32),
             make_activation_layer(RELU),
             make_connected_layer(512, 10),
             make_activation_layer(SOFTMAX)]
@@ -21,13 +24,18 @@ print
 
 print("making model...")
 batch = 128
-iters = 500
-rate = .01
+iters = 300
+rate = .1
 momentum = .9
 decay = .005
 
 m = conv_net()
 print("training...")
+train_image_classifier(m, train, batch, iters, rate, momentum, decay)
+print("done")
+print("training...")
+iters = 200
+rate = .01
 train_image_classifier(m, train, batch, iters, rate, momentum, decay)
 print("done")
 print
@@ -37,4 +45,5 @@ print("training accuracy: %f", accuracy_net(m, train))
 print("test accuracy:     %f", accuracy_net(m, test))
 
 # 7.6 Question: What do you notice about training the convnet with/without batch normalization? How does it affect convergence? How does it affect what magnitude of learning rate you can use? Write down any observations from your experiments:
-# TODO: Your answer
+
+# With the same setting of all hyperparameters, without batch normalization, I got a test accuracy of 0.402; with batch normalization, I got a test accuracy of 0.541. I could see that with batch normalization the training requires less iterations to converge to a loss value, so batch normalization accelerates convergence. I could use larger learning rate with batch normalization because the training is more stable. With the same setting other hyperparameters, when I used learning rate as .1 for 300 iterations, and then learning rate as .01 for 200 iterations (the total number of iterations is still 500), I got a test accuracy of 0.553, which was the best I could get.
